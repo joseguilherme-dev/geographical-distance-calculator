e6f69a00e12ee73a63ac79a333e9f6054d1770e5
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-06 16:40:57 -0300

    :bulb: Add remove and create functions

--- places/urls.py
+++ places/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view()),
+    path('', views.Home.as_view(), name='home'),
     path('places_data/', views.places_dataset, name='data'),
+    path('register_new_house', views.register_new_house, name='register_new_house'),
+    path('delete_house/(?P<id>[0-9]+)/$', views.delete_house, name='delete_house')
 ]
--- places/views.py
+++ places/views.py
@@ -1,35 +1,44 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from django.contrib.gis.geos import fromstr, Point 
 from django.contrib.gis.db.models.functions import Distance
 from geopy.distance import distance as geopy_distance
 
-from .models import Myplaces
+from .models import Houses
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 from django.core.serializers import serialize
 
 
 # Default Location
-latitude = -47.9363899
-longitude = -15.8293825
-institute = (-47.9363899, -15.8293825)
+LAT = -47.9363899
+LON = -15.8293825
+INSTITUTE = (-47.9363899, -15.8293825)
 
-class Home(generic.ListView):
-    model = Myplaces
-    
-    queryset = Myplaces.objects.all()
+class Home(View):
+    def get(self, request):
+        houses = Houses.objects.all()
 
-    new_queryset = [[query.name, round(geopy_distance(query.coords(), institute).kilometers, 1)] for query in queryset]
-    
-    queryset = new_queryset
-
-    context_object_name = 'people'
-
-    template_name = 'home.html'
+        parsed_houses = {}
+        if houses:
+            parsed_houses = [[house.id, house.name, round(geopy_distance(house.coords(), INSTITUTE).kilometers, 1)] for house in houses]
+        
+        return render(request, 'home.html', {'houses': parsed_houses})
 
 def places_dataset(request):
-    place = serialize('geojson', Myplaces.objects.all())
-
+    place = serialize('geojson', Houses.objects.all())
     return HttpResponse(place, content_type='json')
+
+def register_new_house(request):
+    name = request.POST.get('name')
+    lat = float(request.POST.get('lat'))
+    lon = float(request.POST.get('lon'))
+    new_house = Houses.objects.create(name=name, location=Point(lat, lon))
+    new_house.save()
+    return HttpResponseRedirect(reverse('home'))
+    
+def delete_house(request, id=None):
+    instance = Houses.objects.get(id=id).delete()
+    return HttpResponseRedirect(reverse('home'))
